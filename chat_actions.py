e277e9c83f692d757185abec92421c04851b164d
cthulkhu <cthulkhu@gmail.com>
2021-01-07 09:10:43 +0200

    fixed ban time

--- chat_actions.py
+++ chat_actions.py
@@ -7,4 +7,7 @@ import telegram
 def r_msg_freq(update, context, secs):
     update.message.reply_text("Астанавитесь!")
     perms = telegram.ChatPermissions(can_send_messages=False, can_send_media_messages=False, can_send_polls=False, can_send_other_messages=False, can_add_web_page_previews=False, can_change_info=False, can_invite_users=False, can_pin_messages=False)
-    context.bot.restrict_chat_member(update.message.chat.id, update.message.from_user.id, perms, (datetime.datetime.now() - datetime.timedelta(hours=3) + datetime.timedelta(seconds=secs)))
+    context.bot.restrict_chat_member(update.message.chat.id, update.message.from_user.id, perms, (update.message.date + datetime.timedelta(seconds=secs)))
+
+
+
